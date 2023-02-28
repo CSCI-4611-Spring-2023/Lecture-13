3cc4864d796bf29c8794232cc7e2e2139093dbc8
Evan Suma Rosenberg <suma@umn.edu>
2023-02-28 17:00:11 -0600

    class commit

--- src/MeshViewer.ts
+++ src/MeshViewer.ts
@@ -9,12 +9,14 @@ import * as gfx from 'gophergfx'
 export class MeshViewer extends gfx.GfxApp
 {
     private cameraControls: gfx.OrbitControls;
+    private character: gfx.Transform3;
 
     constructor()
     {
         super();
 
         this.cameraControls = new gfx.OrbitControls(this.camera);
+        this.character = new gfx.Transform3();
     }
 
     createScene(): void 
@@ -41,10 +43,40 @@ export class MeshViewer extends gfx.GfxApp
         ground.material.setColor(new gfx.Color(0, 0.5, 0.5));
         ground.position.y = -0.5;
         this.scene.add(ground);
+
+        
     }
 
     update(deltaTime: number): void 
     {
         this.cameraControls.update(deltaTime);
+    }
+
+    private loadMorphMesh(meshFile1: string, meshFile2: string, textureFile: string): gfx.MorphMesh
+    {
+        // Create morph mesh
+        const morphMesh = new gfx.MorphMesh();
+
+        // Load and copy buffer data from the first mesh into the buffers
+        gfx.ObjLoader.load(meshFile1, (loadedMesh: gfx.Mesh)=>{
+            morphMesh.positionBuffer = loadedMesh.positionBuffer;
+            morphMesh.normalBuffer = loadedMesh.normalBuffer;
+            morphMesh.texCoordBuffer = loadedMesh.texCoordBuffer;
+            morphMesh.colorBuffer = loadedMesh.colorBuffer;
+            morphMesh.indexBuffer = loadedMesh.indexBuffer;
+            morphMesh.vertexCount = loadedMesh.vertexCount;
+            morphMesh.triangleCount = loadedMesh.triangleCount;
+        });
+
+        // Load and copy buffer data from the second mesh into the morph buffers
+        gfx.ObjLoader.load(meshFile2, (loadedMesh: gfx.Mesh)=>{
+            morphMesh.morphTargetPositionBuffer = loadedMesh.positionBuffer;
+            morphMesh.morphTargetNormalBuffer = loadedMesh.normalBuffer;
+        });
+
+        // Load the texture and assign it to the material
+        morphMesh.material.texture = new gfx.Texture(textureFile);
+
+        return morphMesh;
     }
 }
